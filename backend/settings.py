f038229ca03c2e08a385e946d46c324a633ec78e
Sergey Golstinin <golstinin@gmail.com>
2026-04-25 17:41:17 -0400

    Switch from SQLite to PostgreSQL database

--- backend/settings.py
+++ backend/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'job_tracker',
+        'USER': 'job_tracker_user',
+        'PASSWORD': 'yourpassword',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
