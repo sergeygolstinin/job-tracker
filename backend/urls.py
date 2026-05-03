3d48f2e881ac71b25652765fd34e55ebb5c8a77e
Sergey Golstinin <golstinin@gmail.com>
2026-05-03 15:46:54 -0400

    Remove debug endpoint and disable DEBUG in production

--- backend/urls.py
+++ backend/urls.py
@@ -1,18 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.http import JsonResponse
-from django.conf import settings
-
-def debug_settings(request):
-    return JsonResponse({
-        'CSRF_TRUSTED_ORIGINS': settings.CSRF_TRUSTED_ORIGINS,
-        'ALLOWED_HOSTS': settings.ALLOWED_HOSTS,
-        'DEBUG': settings.DEBUG,
-        'MIDDLEWARE': settings.MIDDLEWARE,
-    })
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('jobs.urls')),
-    path('debug-settings/', debug_settings),
 ]
